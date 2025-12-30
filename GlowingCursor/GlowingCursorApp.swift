c98c8cd409cd732f70f3f68f5739415ccdb3582b
Josiah Royal <119104108+mechobliterate@users.noreply.github.com>
2025-12-30 18:22:03 -0500

    Initial Commit

---
+++ GlowingCursor/GlowingCursorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlowingCursorApp.swift
+//  GlowingCursor
+//
+//  Created by Josiah Royal on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct GlowingCursorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
